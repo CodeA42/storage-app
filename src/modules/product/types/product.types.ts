1377327a7aa70cc6507ec1a964a3e4612a72b049
alexander.velichkov <alexander.velichkov@pytek.bg>
2023-01-22 12:26:19 +0200

    Add product types

---
+++ src/modules/product/types/product.types.ts
@@ -0,0 +1,49 @@
+import { ApiProperty } from "@nestjs/swagger";
+import {
+  IsAlphanumeric,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+} from "class-validator";
+
+export enum ProductCategory {
+  stationery = "stationery",
+  groceries = "groceries",
+  materials = "materials",
+}
+
+export class CreateProductDto {
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsAlphanumeric()
+  name: string;
+
+  @ApiProperty()
+  @IsOptional()
+  description?: string;
+
+  @ApiProperty()
+  @IsOptional()
+  image?: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  purchasePrice: number;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  salePrice: number;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  @IsPositive()
+  count: number;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  category: ProductCategory;
+}
