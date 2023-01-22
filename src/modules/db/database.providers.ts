60df8e0bf3270570e2e5ccf9aaf40984f24dad21
alexander.velichkov <alexander.velichkov@pytek.bg>
2023-01-22 14:11:10 +0200

    Add product entity

--- src/modules/db/database.providers.ts
+++ src/modules/db/database.providers.ts
@@ -1,11 +1,12 @@
-import { join } from 'path';
-import { getEnvFilePath } from 'src/config/envFilePath';
-import { database } from 'src/config/types';
-import * as dotenv from 'dotenv';
-import { DataSource, DataSourceOptions } from 'typeorm';
-import { DatabaseKeys } from 'src/@types/app.types';
-import User from '../user/entities/user.entity';
-import Token from '../authentication/entities/token.entity';
+import { join } from "path";
+import { getEnvFilePath } from "src/config/envFilePath";
+import { database } from "src/config/types";
+import * as dotenv from "dotenv";
+import { DataSource, DataSourceOptions } from "typeorm";
+import { DatabaseKeys } from "src/@types/app.types";
+import User from "../user/entities/user.entity";
+import Token from "../authentication/entities/token.entity";
+import Product from "../product/entities/product.entity";
 
 let dataSourceOptions: DataSourceOptions;
 
@@ -25,17 +26,17 @@ export const databaseProviders = [
 export function getDataSource(isCli: boolean): DataSource {
   dotenv.config({ path: getEnvFilePath() });
   const migrationsLocation = isCli
-    ? 'src/modules/db/migrations/*.ts'
-    : join(__dirname, 'modules', 'db', 'migrations', '*.ts');
+    ? "src/modules/db/migrations/*.ts"
+    : join(__dirname, "modules", "db", "migrations", "*.ts");
 
   dataSourceOptions = {
-    type: 'postgres',
+    type: "postgres",
     host: process.env[database.host],
     port: +process.env[database.port],
     username: process.env[database.username],
     password: process.env[database.password],
     database: process.env[database.name],
-    entities: [User, Token],
+    entities: [User, Token, Product],
     migrations: [migrationsLocation],
   };
 
