417ee81f3928325aef92d1b017b0426ba5879cd9
alexander.velichkov <alexander.velichkov@pytek.bg>
2023-01-22 12:52:11 +0200

    Wire up some functionality

--- src/modules/product/product.service.ts
+++ src/modules/product/product.service.ts
@@ -1,26 +1,27 @@
 import { Injectable } from "@nestjs/common";
+import { DeleteResult } from "typeorm";
 import Product from "./entities/product.entity";
 import { ProductRepository } from "./product.repository";
 import { CreateProductDto } from "./types/product.types";
 
 @Injectable()
 export class ProductService {
-  constructor(private readonly userRepository: ProductRepository) {}
+  constructor(private readonly productRepository: ProductRepository) {}
 
   createProduct(product: CreateProductDto): Promise<Product> {
-    throw new Error("Method not implemented.");
+    return this.productRepository.save(product);
   }
 
   updateProduct(product: Partial<Product>): Promise<Product> {
-    throw new Error("Method not implemented.");
+    return this.productRepository.save(product);
   }
 
   getProduct(id: string): Promise<Product> {
-    throw new Error("Method not implemented.");
+    return this.productRepository.findByIdOrFail(id);
   }
 
-  deleteProduct(id: string): Promise<Product> {
-    throw new Error("Method not implemented.");
+  deleteProduct(id: string): Promise<DeleteResult> {
+    return this.productRepository.delete(id);
   }
 
   getProducts(): Promise<Product[]> {
