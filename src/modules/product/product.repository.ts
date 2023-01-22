e0b4dcda6cebd5b6cb5a65d39b29489efa364f67
alexander.velichkov <alexander.velichkov@pytek.bg>
2023-01-22 12:51:56 +0200

    Add find by id of fail

--- src/modules/product/product.repository.ts
+++ src/modules/product/product.repository.ts
@@ -2,6 +2,7 @@ import { Inject, Injectable } from "@nestjs/common";
 import { DatabaseKeys } from "src/@types/app.types";
 import { Repository } from "typeorm";
 import Product from "./entities/product.entity";
+import { ProductNotFoundError } from "./types/product.errors";
 
 @Injectable()
 export class ProductRepository extends Repository<Product> {
@@ -9,5 +10,13 @@ export class ProductRepository extends Repository<Product> {
     @Inject(DatabaseKeys.USER_REPOSITORY) repository: Repository<Product>
   ) {
     super(repository.target, repository.manager, repository.queryRunner);
+  }
+
+  async findByIdOrFail(id: string): Promise<Product> {
+    const product: Product = await this.findOne({
+      where: { id },
+    });
+    if (product) return product;
+    throw new ProductNotFoundError();
   }
 }
